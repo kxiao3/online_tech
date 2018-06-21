63e12c8c98f79d7793c07d438123af542766127b
unknown <xiaokai1@XIAOKAI1-04RWM6.lenovo.com>
2018-06-21 14:22:59 +0800

    Initial commit

---
+++ apps/__init__.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python  
+# -*- coding: utf-8 -*-
+'''
+-------------------------------------------------
+   File Name：     __init__.py
+   Description :
+   Author :       xiaokai1
+   date：          2018/6/17
+-------------------------------------------------
+'''
+__author__ = 'xiaokai1'
